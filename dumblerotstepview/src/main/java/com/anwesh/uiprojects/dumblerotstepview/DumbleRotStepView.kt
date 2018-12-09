24dcb1a9946ae52ff9c8b20dc723626c62599783
Anwesh43 <anweshthecool0@gmail.com>
2018-12-09 11:25:39 +0530

    adding drawDRSNode canvas extension method

--- dumblerotstepview/src/main/java/com/anwesh/uiprojects/dumblerotstepview/DumbleRotStepView.kt
+++ dumblerotstepview/src/main/java/com/anwesh/uiprojects/dumblerotstepview/DumbleRotStepView.kt
@@ -6,12 +6,9 @@ package com.anwesh.uiprojects.dumblerotstepview
 
 import android.view.View
 import android.view.MotionEvent
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
-import android.graphics.RectF
 import android.app.Activity
 import android.content.Context
+import android.graphics.*
 
 val nodes : Int = 5
 val lines : Int = 4
@@ -19,6 +16,9 @@ val sizeFactor : Float = 2.4f
 val strokeFactor : Int = 90
 val scDiv : Double = 0.51
 val scGap : Float = 0.05f
+val hSize : Float = 0.8f
+val color : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.getInverse() : Float = 1f / this
 
@@ -29,3 +29,32 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawDRSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val rectH : Float = hSize * size
+    val triSize : Float = size - rectH
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = color 
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(sc2 * 90f)
+    drawRect(RectF(-size, -triSize, size, triSize), paint)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        val dy : Float = size
+        val oy : Float = rectH
+        val sy : Float = oy + (dy - oy) * sc
+        val path: Path = Path()
+        path.moveTo(rectH, -rectH)
+        path.lineTo(size, -rectH)
+        path.lineTo(size, -sy)
+        path.lineTo(rectH, -rectH)
+        drawPath(path, paint)
+    }
+    restore()
+}
