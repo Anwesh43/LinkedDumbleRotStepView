70d26b5a88cc911b2e921246a8341c94869cbec6
Anwesh43 <anweshthecool0@gmail.com>
2018-12-09 11:13:45 +0530

    adding libraries and defining constants

--- dumblerotstepview/src/main/java/com/anwesh/uiprojects/dumblerotstepview/DumbleRotStepView.kt
+++ dumblerotstepview/src/main/java/com/anwesh/uiprojects/dumblerotstepview/DumbleRotStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.dumblerotstepview
 /**
  * Created by anweshmishra on 09/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val sizeFactor : Float = 2.4f
+val strokeFactor : Int = 90
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
