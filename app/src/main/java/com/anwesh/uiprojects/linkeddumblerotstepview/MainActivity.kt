f8d68f8e16eceaf205bb3a1381e43fb7b510b1a7
Anwesh43 <anweshthecool0@gmail.com>
2018-12-09 11:48:58 +0530

    adding DumbleRotStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddumblerotstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddumblerotstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkeddumblerotstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.dumblerotstepview.DumbleRotStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DumbleRotStepView.create(this)
     }
 }
